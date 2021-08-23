403a2f2d5c1c503478cb917f7fb07596121f393e
khnoh <noh5524@gmail.com>
2021-08-23 23:02:50 +0900

    processor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" :src="hehe" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
-  awdawd
+  <div class="test">
+    <span class="test__one">awdasfewd</span>
+    <span class="test__two">{{test}}44</span>
+  </div>
+  
+  
+  <!-- {{hehe}} -->
+  <!-- {{import.meta.env.MODE}} -->
 </template>
 
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
+import imgUrl from '../public/favicon.ico';
+
+
+export default {
+  components: {HelloWorld},
+  data() {
+    return {
+      test: 1,
+      hehe: imgUrl
+    }
+  },
+  created() {
+    // env meta tag 접근
+    console.log(import.meta.env)
+    // this.hehe = import.meta.env.MODE;
+  }
+}
+
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 
-<style>
+<style lang="scss" scoped>
+/* scss - yarn add -D sass 만 하고 lang 추가만하면됨 node-sass 설치할 필요없음 */
+.test {
+  display: flex;
+  justify-content: center;
+  &__one {
+    color: red;
+  }
+  &__two {
+    color: blue;
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
